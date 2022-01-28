188735b4ffa2511bafc247e9eb66e6886513e7f7
irishamargo <97751041+irishamargo@users.noreply.github.com>
2022-01-28 19:07:09 +0300

    Update pin-pong.py
    Добавлен мячик

--- pin-pong.py
+++ pin-pong.py
@@ -9,6 +9,12 @@ display.set_caption('Пин-понг')
 #задай фон сцены
 background = transform.scale(image.load('fon.jpg'), (win_width, win_height))
 
+#текст
+font.init()
+font = font.Font(None, 70)
+lose1= font.render('Player 1 lose!', True, (180, 0, 0))
+lose2 = font.render('Player 2 lose!', True, ( 180, 0, 0))
+
 #создание класса
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -39,27 +45,50 @@ class Player(GameSprite):
 #игроки
 player1 = Player('roc.png', win_width - 80, win_height/2-20, 50, 100, 9)
 player2 = Player('roc.png', 30, win_height/2-20, 50, 100, 9)
+ball = GameSprite('krug1.png', 350, 250, 40, 40, 9)
+
+speedx = 3
+speedy = 3
+
+
 
 clock = time.Clock()
 FPS = 60
 
-run = True
+game = True
 finish = False
 
-while run:
+while game:
 
     for e in event.get():
         if e.type == QUIT:
-            run = False
+            game = False
     
     if finish != True:
         window.blit(background, (0, 0))
 
         player1.update1()
         player2.update2()
+
+        ball.rect.x += speedx
+        ball.rect.y += speedy
+
+        if ball.rect.y < 10 or ball.rect.y > win_height - 50:
+            speedy *= -1
+
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speedx *= -1
+            speedy *= 1
+
+        if ball.rect.x < 0:
+            window.blit(lose1, (200, 200))
+        
+        if ball.rect.x > win_width:
+            window.blit(lose2, (200, 200))
         
         player1.reset()
         player2.reset()
+        ball.reset()
 
     display.update()
     clock.tick(FPS)
